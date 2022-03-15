50a000a4ef462fbfb2271f65f62f33d7c29ba138
anirudh-31 <betrabetanirudh@gmail.com>
2022-03-15 19:27:01 +0530

    added copy functionality

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Palatte palatte={seed[5]} />
+        <Palatte palatte={seed[4]} />
       </div>
     );
   }
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,17 +1,24 @@
 import React, { Component } from "react";
 import "./ColorBox.css";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 export default class ColorBox extends Component {
   constructor(props) {
     super(props);
   }
   render() {
+    const { name, background } = this.props;
     return (
-      <div
-        className="ColorBox"
-        style={{ backgroundColor: this.props.background }}
-      >
-        <span>{this.props.name}</span>
-      </div>
+      <CopyToClipboard text={background}>
+        <div className="ColorBox" style={{ backgroundColor: background }}>
+          <div className="copy-container">
+            <div className="content">
+              <span>{name}</span>
+            </div>
+            <button className="copy-button">Copy</button>
+          </div>
+          <span className="see-more">MORE</span>
+        </div>
+      </CopyToClipboard>
     );
   }
 }
